f55966e77778c093403134f7a9d9141fecfff323
Adam Black <fernando@notoriovs.com>
2025-03-24 19:22:27 -0600

    first commit

---
+++ info.js
@@ -0,0 +1,31 @@
+export const info = {
+  legalName: "BLUE HAND S.A. DE C.V.",
+  companyName: "BH Consulting",
+  description: "Certificate en ISO",
+  email: {
+    sender: "",
+    recipients: [""],
+    subject: "Nuevo prospecto BH Consulting",
+  },
+  phoneNumber: "+525549118039",
+  whatsapp: {
+    value: "+525549118039",
+    message: "Buen día, me puede dar más información sobre las capacitaciones",
+  },
+  social: {
+    facebook: "",
+    instagram: "",
+  },
+  address: {
+    address: "",
+    col: "",
+    cp: "",
+    city: "",
+    state: "",
+  },
+  optInWebhook: '',
+  surveyWebhook: '',
+  surveyRedirect: '',
+  privacyNotice: "/aviso-privacidad",
+  termsConditions: "/aviso-privacidad",
+};
